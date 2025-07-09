f4e571eb25240a0ee9937fab99782f6bdf32a100
kim <wjdgus9033@naver.com>
2025-07-09 15:02:57 +0900

    사이드바 변경

--- src/components/root/root.jsx
+++ src/components/root/root.jsx
@@ -1,5 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import { useState } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./root.css";
 
 export default function Root() {
@@ -7,22 +8,44 @@ export default function Root() {
 
   return (
     <div className="container">
-      <nav className="sidebar">
-        <Link to="/">홈</Link>
-        <Link to="/state">피해 통계</Link>
-        <Link to="/shelters">쉼터 찾기</Link>
+      <aside className="side-bar">
+        <section className="side-bar__icon-box">
+          <section className="side-bar__icon-1">
+            <div></div><div></div><div></div>
+          </section>
+        </section>
 
-        <button className="info-button" onClick={() => setInfoOpen(!infoOpen)}>
-          정보 안내
-        </button>
-        {infoOpen && (
-          <div className="info-submenu">
-            <Link to="/about/heat wave">폭염이란?</Link>
-            <Link to="/about/precautions">예방 수칙 안내</Link>
-            <Link to="/about/symptoms">증상별 대처법</Link>
-          </div>
-        )}
-      </nav>
+        <ul>
+          <li>
+            <Link to="/">
+              <i className="fa-solid fa-house"></i> 홈
+            </Link>
+          </li>
+          <li>
+            <Link to="/state">
+              <i className="fa-solid fa-chart-line"></i> 피해 통계
+            </Link>
+          </li>
+          <li>
+            <Link to="/shelters">
+              <i className="fa-solid fa-building-shield"></i> 쉼
+              터 찾기
+            </Link>
+          </li>
+          <li>
+            <button className="info-button" onClick={() => setInfoOpen(!infoOpen)}>
+              <i className="fa-solid fa-circle-info"></i> 정보 안내
+            </button>
+            {infoOpen && (
+              <ul className="info-submenu">
+                <li><Link to="/about/heat wave">폭염이란?</Link></li>
+                <li><Link to="/about/precautions">예방 수칙 안내</Link></li>
+                <li><Link to="/about/symptoms">증상별 대처법</Link></li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </aside>
 
       <main className="main-content">
         <Outlet />
